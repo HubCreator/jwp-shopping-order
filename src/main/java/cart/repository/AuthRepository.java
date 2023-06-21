8907fc0ce7196223335e6dd20823c3bf373d59f8
HubCreator <chocomilk8604@gmail.com>
2023-06-21 15:52:06 +0900

    refactor: AuthRepository 수정

--- src/main/java/cart/repository/AuthRepository.java
+++ src/main/java/cart/repository/AuthRepository.java
@@ -3,21 +3,27 @@ package cart.repository;
 import cart.domain.auth.Auth;
 import cart.domain.member.Member;
 import cart.domain.member.MemberEmail;
+import cart.exception.notfound.MemberNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 
 @Repository
 @RequiredArgsConstructor
 public class AuthRepository {
 
     private final EntityManager em;
-    private final MemberRepository memberRepository;
-
 
     public Auth findByEmail(final MemberEmail email) {
-        final Member member = memberRepository.findByEmail(email);
-        return new Auth(member.getEmail(), member.getPassword());
+        try {
+            final Member member = em.createQuery("select m from Member m where m.email = :email", Member.class)
+                    .setParameter("email", email)
+                    .getSingleResult();
+            return new Auth(member.getEmail(), member.getPassword());
+        } catch (final NoResultException e) {
+            throw new MemberNotFoundException(email, e);
+        }
     }
 }
